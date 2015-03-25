bbce178c1b3aea086cf0e7ab0e1bbbeb19313365
Phillip Stenger <pstenger@nd.edu>
2015-03-25 00:38:21 -0400

    Minor Changes

--- Link.h
+++ Link.h
@@ -1,7 +1,8 @@
+//This is the derived class for the character link
 #ifndef LINK_H
 #define LINK_H
 #include <SDL2/SDL.h>
-#include "Character.h"
+#include "Character.h"  //base class
 #include <stdio.h>
 #include <iostream>
 using namespace std;
@@ -31,13 +32,11 @@ bool Link::init() {
     success=false;
   } else {
     gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT , SDL_WINDOW_SHOWN );
-    if( gWindow == NULL )
-      {
+    if( gWindow == NULL ){
 	printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
 	success=false;
       }
-    else
-      {
+    else{
 
 	    //Get window surface
 	    gScreenSurface = SDL_GetWindowSurface( gWindow );
@@ -47,15 +46,16 @@ bool Link::init() {
   return success;
 }
 
-bool Link::loadMedia()
-{
+
+bool Link::loadMedia(){
+
   //Loading success flag
   bool success = true;
 
   //Load default surface
   gImage = loadSurface( imagename );
-  if( gImage == NULL )
-    {
+  if( gImage == NULL ){
+
       printf( "Failed to load default image!\n" );
       success = false;
     }
@@ -73,16 +73,15 @@ void Link::close(){
 
 }
 
-SDL_Surface* Link::loadSurface( string path )
-{
 
+SDL_Surface* Link::loadSurface( string path ){
   //The final optimized image
   SDL_Surface* optimizedSurface = NULL;
 
   //Load image at specified path
   SDL_Surface* loadedSurface = SDL_LoadBMP( path.c_str() );
-  if( loadedSurface == NULL )
-    {
+  if( loadedSurface == NULL ){
+
       cout<<"Unable to load image"<<SDL_GetError()<<"\n";
     }
   else {
--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
+//simple driver program to run the functions
 #include <iostream>
 #include "Character.h"
 #include "Link.h"
 using namespace std;
 
-int main (){ // ( int argc, char* args[] ) {
+
+int main (){ 
   Link link1;
   Character * character1= &link1;
-  // character1->runfunc();
-  character1->runfunc();
+  character1->runfunc(); //runs runfunc for link1 object
+
   return 0;
 }
